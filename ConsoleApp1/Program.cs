f017b26a866379639fdc2de8f55c8960e11035e1
Denys Shaitan <schaitan.den@gmail.com>
2025-01-13 17:29:24 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+
+class Employee
+{
+
+    // Поля для збереження прізвища та імені співробітника
+    private string lastName;
+    private string firstName;
+
+    // Поля для посади та стажу
+    private string position;
+    private int experience;
+
+    // Конструктор
+    public Employee(string lastName, string firstName, string position, int experience)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.position = position;
+        this.experience = experience;
+    }
+
+    // Метод для розрахунку окладу
+    public double CalculateSalary()
+    {
+        double baseSalary;
+
+        // Визначення базового окладу залежно від посади
+        switch (position.ToLower())
+        {
+            case "менеджер":
+                baseSalary = 1000;
+                break;
+            case "інженер":
+                baseSalary = 800;
+                break;
+            case "робітник":
+                baseSalary = 600;
+                break;
+            default:
+                baseSalary = 500;
+                break;
+        }
+
+        // Додавання надбавки за стаж
+        double experienceBonus = experience * 50;
+        return baseSalary + experienceBonus;
+    }
+
+    // Метод для розрахунку податкового збору
+    public double CalculateTax()
+    {
+        double salary = CalculateSalary();
+        return salary * 0.2; // 20% податковий збір
+    }
+
+    // Метод для виведення інформації про співробітника
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Прізвище: {lastName}");
+        Console.WriteLine($"Ім'я: {firstName}");
+        Console.WriteLine($"Посада: {position}");
+        Console.WriteLine($"Оклад: {CalculateSalary():F2}");
+        Console.WriteLine($"Податковий збір: {CalculateTax():F2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding +
+
+        // Зчитування інформації про співробітника від користувача
+        Console.Write("Введіть прізвище співробітника: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Введіть ім'я співробітника: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введіть посаду співробітника: ");
+        string position = Console.ReadLine();
+
+        Console.Write("Введіть стаж роботи (у роках): ");
+        int experience = int.Parse(Console.ReadLine());
+
+        // Створення об'єкта класу Employee
+        Employee employee = new Employee(lastName, firstName, position, experience);
+
+        // Виведення інформації про співробітника
+        Console.WriteLine("\nІнформація про співробітника:");
+        employee.DisplayInfo();
+    }
+}
---
+++ MoneyExchanger/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+
+namespace MoneyExchanger
+{
+    internal class Program
+    {
+        class Conventer
+        {
+            private double UsdToUah;
+            private double EurToUah;
+            private double GbtToUah;
+
+            public Conventer(double USD, double EUR, double GBT)
+            {
+                UsdToUah = USD;
+                EurToUah = EUR;
+                GbtToUah = GBT;
+            }
+
+            //з гривні у валюту   amount=кількість
+            public double ConvertFromUah(double amount, string currency)
+            {
+                return currency.ToLower() switch
+                {
+                    "USD" => amount / UsdToUah,
+                    "EUR" => amount / EurToUah,
+                    "GBT" => amount / GbtToUah,
+                    _ => throw new ArgumentException("Невідома валюта")
+                };
+            }
+
+            //з валюти у грн
+            public double ConvertToUah(double amount, string currency)
+            {
+                return currency.ToLower() switch
+                {
+                    "USD" => amount * UsdToUah,
+                    "EUR" => amount * EurToUah,
+                    "GBT" => amount * GbtToUah,
+                    _ => throw new ArgumentException("Невідома валюта")
+                };
+            }
+
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.Unicode;
+
+                Console.WriteLine("Напишіть курс долара до гривні:");
+                double usdRate = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Напишіть курс евро до гривні:");
+                double eurRate = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Напишіть курс стерлінга до гривні:");
+                double gbtRate = double.Parse(Console.ReadLine());
+
+                Conventer conventer = new Conventer(usdRate, eurRate, gbtRate);
+
+                while (true)
+                {
+                    Console.WriteLine("Виберіть операцію:");
+                    Console.WriteLine("1 - Конвертувати з гривні у валюту");
+                    Console.WriteLine("2 - Конвертувати з валюти у гривню");
+                    Console.WriteLine("3 - Вихід");
+
+                    string choice = Console.ReadLine();
+
+                    if (choice == "3") break;
+
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.WriteLine("Введіть суму у гривнях:");
+                            double uahAmount = double.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Виберіть валюту:USD,EUR,GBT");
+                            string currency = Console.ReadLine();
+                            break;
+
+                        case "2":
+                            Console.WriteLine("Введіть суму у валюті:");
+                            double valutaAmount = double.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Виберіть валюту:USD,EUR,GBT");
+                            string toCurrency = Console.ReadLine();
+                            break;
+
+                        default:
+                            Console.WriteLine("Виникла помилка, спробуйте ще раз!");
+                            break;
+
+
+
+                    }
+                }
+
+                Console.WriteLine("До зустрічі!");
+
+            }
+
+
+
+        }
+    }
+}
